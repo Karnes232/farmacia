6dda7b315e773588cd650628c5c4da94d36829dc
James Karnes <karnes.james@gmail.com>
2020-10-03 14:42:34 -0400

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,14 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 's8t0w_7ksz7q=&xd1+a!+ng4eilo^b(xv@2ctk1qwi@n-nbc%5'
 
-import os 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 's8t0w_7ksz7q=&xd1+a!+ng4eilo^b(xv@2ctk1qwi@n-nbc%5')
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+#import os 
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 's8t0w_7ksz7q=&xd1+a!+ng4eilo^b(xv@2ctk1qwi@n-nbc%5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['farmacia.eba-yt7qehpz.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['farmacia-melo.herokuapp.com']
 
 
 # Application definition
